8e800b5977e1bae4aa95dccfb6671e4ddc62625d
Shafiqul Islam <www.shafiquebd@gmail.com>
2025-07-06 14:51:16 +0600

    springboot fundamenta project initailise

---
+++ spring-boot-fundamental/src/main/java/com/java_project/spring_boot_fundamental/SpringBootFundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.java_project.spring_boot_fundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFundamentalApplication.class, args);
+	}
+
+}
---
+++ spring-boot-fundamental/src/test/java/com/java_project/spring_boot_fundamental/SpringBootFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_project.spring_boot_fundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
